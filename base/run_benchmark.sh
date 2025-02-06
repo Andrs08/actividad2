fe1375d5caacfbee792db698573be1842244511e
Andrs08 <chinchillahuelvas@gmail.com>
2025-02-06 16:09:39 -0500

    automatizacion del benchmark

--- base/run_benchmark.sh
+++ base/run_benchmark.sh
@@ -6,21 +6,25 @@ git clone https://github.com/Andrs08/actividad2.git /benchmark
 # Entra al directorio clonado
 cd /benchmark/lenguajes
 
+# Archivo donde se guardarán los resultados
+mkdir -p /benchmark/resultados
+RESULTS_FILE="/benchmark/resultados/tiempos.csv"
+echo "Lenguaje,Tiempo (ms)" > $RESULTS_FILE
+
 # Recorre cada carpeta de lenguaje
 for lang in */ ; do
-    echo "Ejecutando benchmark en $lang"
+    lang_clean=${lang%/}  # Elimina la barra al final del nombre
+    echo "Ejecutando benchmark en $lang_clean"
     
     # Construye la imagen del lenguaje
-    docker build -t benchmark-$lang $lang
+    docker build -t benchmark-$lang_clean $lang_clean
     
-    # Ejecuta el contenedor y mide el tiempo
-    tiempo=$(docker run --rm benchmark-$lang)
+    # Ejecuta el contenedor y guarda el tiempo
+    tiempo=$(docker run --rm benchmark-$lang_clean)
     
-    # Guarda el resultado en la carpeta de resultados
-    echo "$lang,$tiempo" >> /benchmark/resultados/tiempos.csv
+    # Guarda el resultado en el archivo
+    echo "$lang_clean,$tiempo" >> $RESULTS_FILE
 done
 
-# Muestra los resultados
-cat /benchmark/resultados/tiempos.csv
-
-
+# Mostrar los resultados
+cat $RESULTS_FILE
